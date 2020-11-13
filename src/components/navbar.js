ae4854f47ed19bf1d1119855d8dcff444c3a5c2a
kellenbusby <kellenbusby@gmail.com>
2020-11-13 12:21:53 -0700

    Initial changes to starter

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,41 +1,30 @@
-import React from 'react';
-import { FaGithub } from 'react-icons/fa';
+import React from "react";
+import { FaGithub } from "react-icons/fa";
 
-import './style.scss';
-import gatsbyLogo from '../images/gatsby-icon.png';
-import bulmaLogo from '../images/bulma-logo.png';
+import "./style.scss";
 
 const Navbar = () => (
-	<div className="hero-head is-hidden-mobile">
-		<nav className="navbar">
-			<div className="container">
-				<div className="navbar-brand">
-					<a
-						className="navbar-item"
-						href="https://github.com/amandeepmittal/gatsby-bulma-quickstart"
-					>
-						<img src={gatsbyLogo} alt="Logo-1" />
-						<img src={bulmaLogo} alt="Logo-2" />
-					</a>
-				</div>
-				<div id="navbarMenuHeroA" className="navbar-menu">
-					<div className="navbar-end">
-						<span className="navbar-item">
-							<a
-								className="button is-danger is-inverted"
-								href="https://github.com/amandeepmittal/gatsby-bulma-quickstart"
-							>
-								<span className="icon">
-									<FaGithub size="fa-2x" />
-								</span>
-								<span>Download</span>
-							</a>
-						</span>
-					</div>
-				</div>
-			</div>
-		</nav>
-	</div>
+  <div className="hero-head is-hidden-mobile">
+    <nav className="navbar">
+      <div className="container">
+        <div id="navbarMenuHeroA" className="navbar-menu">
+          <div className="navbar-end">
+            <span className="navbar-item">
+              <a
+                className="button is-ghost is-inverted"
+                href="https://github.com/busbyk"
+              >
+                <span className="icon">
+                  <FaGithub size="fa-2x" />
+                </span>
+                <span>busbyk</span>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
 );
 
 export default Navbar;
