88df0402680a89d44c540d6f6a991a3c2a2bbfef
Kellen Busby <kellenbusby@gmail.com>
2021-06-03 15:12:41 -0400

    redesign of landing page with typing effects and cta animations

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,10 @@ import React from 'react'
 import './style.scss'
 import './Typist.css'
 import Helmet from './helmet'
-import Navbar from './navbar'
 
 const Layout = ({ children }) => (
   <div>
     <Helmet />
-    <Navbar />
     {children}
   </div>
 )
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,38 +1,104 @@
 import React, { useState } from 'react'
 import Typist from 'react-typist'
+import { FaGithub } from 'react-icons/fa'
+import { RiMailSendFill } from 'react-icons/ri'
 
 const Welcome = () => {
-  const [showWorkPhrases, setShowWorkPhrases] = useState(true)
+  const [showWorkPhrases, setShowWorkPhrases] = useState(false)
+  const [showTitle, setShowTitle] = useState(false)
+  const [showGitHubBtn, setShowGitHubBtn] = useState(false)
+  const [showCta, setShowCta] = useState(false)
 
   return (
-    <section className='hero gradientBg'>
+    <section className='hero gradientBg is-vh-height'>
       <div className='hero-body'>
         <div className='container'>
-          <div id='typing'>
-            {/* <Typist
-              onTypingDone={() => {
-                setShowWorkPhrases(true)
-              }}
+          <div id='welcome'>
+            <Typist
+              className='huge hi-im'
               cursor={{ hideWhenDone: true }}
-            > */}
-            <span className='huge'>Hey!</span>
-            <br />
-            {/* <Typist.Delay ms={500} /> */}
-            <span className='huge'>I'm Kellen Busby</span>
-            <br />
-            {/* <Typist.Delay ms={500} /> */}
-            <span>I'm a software developer</span>
-            <br />
-            {/* </Typist> */}
-            {showWorkPhrases && <ThingsIBuild />}
+              onTypingDone={() => {
+                setTimeout(() => {
+                  setShowTitle(true)
+                }, 1000)
+              }}
+            >
+              <span>Hey!</span>
+              <Typist.Delay ms={1000} />{' '}
+              <span>
+                I'm <br />
+                Kellen Busby
+              </span>
+            </Typist>
+            {showTitle && (
+              <div id='github-btn-row'>
+                <Typist
+                  cursor={{ hideWhenDone: true }}
+                  onTypingDone={() => {
+                    setTimeout(() => {
+                      setShowWorkPhrases(true)
+                    }, 1500)
+                    setTimeout(() => {
+                      setShowGitHubBtn(true)
+                    }, 1000)
+                  }}
+                >
+                  <span>I'm a software developer</span>
+                </Typist>
+                {showGitHubBtn && (
+                  <div className='slide-in-btns-row'>
+                    <a
+                      className='button slide-in-btn is-size-7-mobile'
+                      href='https://github.com/busbyk'
+                      target='_blank'
+                    >
+                      <span className='icon'>
+                        <FaGithub size='fa-2x' />
+                      </span>
+                      <span>busbyk</span>
+                    </a>
+                    <a
+                      className='button slide-in-btn is-size-7-mobile'
+                      href='mailto:kellenbusby@gmail.com'
+                    >
+                      <span className='icon'>
+                        <RiMailSendFill size='fa-2x' />
+                      </span>
+                      <span>Send me an email!</span>
+                    </a>
+                  </div>
+                )}
+              </div>
+            )}
+            {showWorkPhrases && (
+              <ThingsIBuild
+                onTypingDone={() => {
+                  setShowCta(true)
+                }}
+              />
+            )}
           </div>
+          {showCta && (
+            <div className='cta-btn-row'>
+              <a
+                className='button is-medium cta-btn'
+                href='mailto:kellenbusby@gmail.com'
+              >
+                <span className='icon'>
+                  <RiMailSendFill />
+                </span>
+                <span>Let's work together</span>
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </section>
   )
 }
 
-const ThingsIBuild = () => {
+const ThingsIBuild = (props) => {
+  const { onTypingDone } = props
   const workPhrases = [
     'React apps',
     'web scrapers',
@@ -45,14 +111,8 @@ const ThingsIBuild = () => {
     'whatever you need',
   ]
 
-  const [workPhrasesDone, setWorkPhrasesDone] = useState(false)
-
   return (
-    <Typist
-      onTypingDone={() => {
-        setWorkPhrasesDone(true)
-      }}
-    >
+    <Typist className='work-phrases-row' onTypingDone={onTypingDone}>
       <span>I build </span>
       {workPhrases.map((phrase, idx) => (
         <span
