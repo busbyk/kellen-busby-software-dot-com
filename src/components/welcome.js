6f0bf2562bb133b1b4c6fcfb6304de9213c338ce
Kellen Busby <kellenbusby@gmail.com>
2021-07-19 12:14:54 -0400

    removing typing animation from what I build heading

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -5,7 +5,6 @@ import { RiMailSendFill } from 'react-icons/ri'
 import profileImg from '../images/profile_1.jpg'
 
 const Welcome = () => {
-
   return (
     <section className='hero gradientBg is-vh-height'>
       <div className='hero-body'>
@@ -14,41 +13,39 @@ const Welcome = () => {
             <div className='is-flex is-align-items-center'>
               <div className='mr-4 is-flex-shrink-0'>
                 <img className='profile-img' src={profileImg} alt='' />
-
               </div>
               <div>
-              <h1 className='title has-text-white'>Kellen Busby</h1>
-              <h2 className='subtitle has-text-white'>Full Stack JavaScript and Python Developer</h2>
-
+                <h1 className='title has-text-white'>Kellen Busby</h1>
+                <h2 className='subtitle has-text-white'>
+                  Full Stack JavaScript and Python Developer
+                </h2>
               </div>
-
             </div>
           </div>
           <div className='slide-in-btns-row'>
-                    <a
-                      className='button is-link slide-in-btn is-size-7-mobile'
-                      href='https://github.com/busbyk'
-                      target='_blank'
-                    >
-                      <span className='icon'>
-                        <FaGithub size='fa-2x' />
-                      </span>
-                      <span>busbyk</span>
-                    </a>
-                    <a
-                      className='button is-link slide-in-btn is-size-7-mobile'
-                      href='mailto:kellenbusby@gmail.com'
-                    >
-                      <span className='icon'>
-                        <RiMailSendFill size='fa-2x' />
-                      </span>
-                      <span>Send me an email!</span>
-                    </a>
-                  </div>
-                  <div>
-                  <ThingsIBuild/>
-
-                  </div>
+            <a
+              className='button is-link slide-in-btn is-size-7-mobile'
+              href='https://github.com/busbyk'
+              target='_blank'
+            >
+              <span className='icon'>
+                <FaGithub size='fa-2x' />
+              </span>
+              <span>busbyk</span>
+            </a>
+            <a
+              className='button is-link slide-in-btn is-size-7-mobile'
+              href='mailto:kellenbusby@gmail.com'
+            >
+              <span className='icon'>
+                <RiMailSendFill size='fa-2x' />
+              </span>
+              <span>Send me an email!</span>
+            </a>
+          </div>
+          <div>
+            <ThingsIBuild />
+          </div>
         </div>
       </div>
     </section>
@@ -70,23 +67,25 @@ const ThingsIBuild = (props) => {
   ]
 
   return (
-    <Typist className='work-phrases-row' onTypingDone={onTypingDone}>
+    <div className='work-phrases-row'>
       <p className='is-size-4'>What I build: </p>
-      {workPhrases.map((phrase, idx) => (
-        <span
-          className={
-            idx === workPhrases.length - 1 ? 'emphasized-work-phrase' : ''
-          }
-          key={idx}
-        >
-          {phrase}
-          <Typist.Backspace
-            count={idx < workPhrases.length - 1 ? phrase.length : 0}
-            delay={1500}
-          />
-        </span>
-      ))}
-    </Typist>
+      <Typist onTypingDone={onTypingDone}>
+        {workPhrases.map((phrase, idx) => (
+          <span
+            className={
+              idx === workPhrases.length - 1 ? 'emphasized-work-phrase' : ''
+            }
+            key={idx}
+          >
+            {phrase}
+            <Typist.Backspace
+              count={idx < workPhrases.length - 1 ? phrase.length : 0}
+              delay={1500}
+            />
+          </span>
+        ))}
+      </Typist>
+    </div>
   )
 }
 
